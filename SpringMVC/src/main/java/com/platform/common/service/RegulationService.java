a96c052516d61a8245460a6697b2fb1944fb8536
wangxiaoye-git <1203967573@qq.com>
2020-04-05 23:34:31 +0800

    漏提文件
    Signed-off-by: wangxiaoye-git <1203967573@qq.com>

---
+++ SpringMVC/src/main/java/com/platform/common/service/RegulationService.java
@@ -0,0 +1,13 @@
+package com.platform.common.service;
+
+
+public interface RegulationService {
+	/**
+	 * 生成机构代码
+	 * groupno 6位机构代码
+	 * codeType 单号类型
+	 * @return
+	 */
+	String getOrgCode(String groupno,String codeType);
+
+}
